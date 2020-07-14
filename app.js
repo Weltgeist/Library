486fe825a578d28afa8ad7740488395e8be12152
Weltgeist <narcisse118@gmail.com>
2020-07-14 00:06:35 -0400

    added Movie Class

--- app.js
+++ app.js
@@ -51,3 +51,24 @@ class Book extends Media {
   }
 
 } 
+
+class Movie extends Media {
+  constructor(title, director, runtime){
+    super(title);
+    this._director = director;
+    this._runtime = runtime;
+  }
+  get director(){
+    return this._director;
+  }
+  get runtime(){
+    return this._runtime;
+  }
+  set director(director){
+    this._director = director;
+  }
+  set runtime(runtime){
+    this._runtime = runtime;
+  }
+
+}
